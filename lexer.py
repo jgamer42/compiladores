80142160c87d07ec5b24869a37bd3c878dbbb2f9
jaime <jgaimer42@gmail.com>
2021-02-21 20:42:57 -0500

    hotfix

--- lexer.py
+++ lexer.py
@@ -1,5 +1,4 @@
 import sly
-
 class Lexer(sly.Lexer):
     tokens = {
         NUMBER,
@@ -8,21 +7,15 @@ class Lexer(sly.Lexer):
     literals = '+-*/()'
     ignore = ' \t\r'
     ignore_newline = r'\n+'
-
     def ignore_newline(self,t):
         self.lineno += t.value.count("\n")
-
     @_(r'\d+\.?|\d*\.\d+')
     def NUMBER(self,t):
         t.value = int(t.value)
         return t.value
-
-    def FLOAT(self)
-
     def error(self,t):
         print("%s Caracter es ilegal '%s" % (t.lineno. t.value[0]))
         self.index += 1
-
 def main():
     l = Lexer()
     while True:
